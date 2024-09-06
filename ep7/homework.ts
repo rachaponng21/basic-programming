0fcc3ba6467ed743e9f5001a929a40341b2bbd43
rachaponng21 <rachapongprivate21@gmail.com>
2024-09-06 13:11:24 +0700

    homework aboutme

---
+++ ep7/homework.ts
@@ -0,0 +1,11 @@
+const aboutMe = {
+    firstName: 'rachapong',
+    lastName: 'waengchaiyaphum',
+    age: 19,
+    mail: 'rachapongxxx@g.com',
+    phone: '0909xx45',
+    address: 'Liveroool',
+    gender: 'male',
+    zibcode: '2544',
+    road: 'anfield'
+}
